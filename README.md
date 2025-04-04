82172e625730f4e796ce50cfd4e537b0fcbaee75
Fabrice Creuzot <code@luigifab.fr>
2025-04-04 20:00:00 +0000

    Version 3.0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+Stop russian war. **🇺🇦 Free Ukraine!**
+
+# sentry
+
+Updated version of [Sentry](https://github.com/getsentry/sentry) connector for [Maho](https://github.com/MahoCommerce/maho).
+
+For more information, go to https://www.luigifab.fr/maho/sentry (IPv6 is required).
+
+- Current version: 3.0.1 (04/04/2025)
+- Compatibility: Maho 25.x, PHP 7.2 / 7.3 / 7.4 / 8.0 / 8.1 / 8.2 / 8.3 / 8.4
+- License: OSL 3.0 (with the exception of Sentry SDK JS, which is licensed by MIT)
+
+Be careful, for this module, you have to modify the core, read the doc.
+If you like, take some of your time to improve some translations, go to https://bit.ly/2HyCCEc.
---
+++ src/app/code/community/Luigifab/Sentry/Block/Help.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created D/09/04/2023
+ * Updated L/10/03/2025
+ *
+ * Copyright 2012      | Jean Roussel <contact~jean-roussel~fr>
+ * Copyright 2022-2025 | Fabrice Creuzot (luigifab) <code~luigifab~fr>
+ * Copyright 2022-2023 | Fabrice Creuzot <fabrice~cellublue~com>
+ * https://github.com/luigifab/maho-sentry
+ *
+ * This program is free software, you can redistribute it or modify
+ * it under the terms of the Open Software License (OSL 3.0).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty, without even the implied warranty of
+ * merchantability or fitness for a particular purpose. See the
+ * Open Software License (OSL 3.0) for more details.
+ */
+
+class Luigifab_Sentry_Block_Help extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface {
+
+	public function render(Varien_Data_Form_Element_Abstract $element) {
+
+		$msg = '<a href="https://github.com/luigifab/maho-sentry" style="margin:0;">github.com/luigifab/maho-sentry</a>';
+
+		$src = 'app/code/core/Mage/Core/Model/App.php';
+		$src = is_file($src) ? $src : 'vendor/mahocommerce/maho/app/code/core/Mage/Core/Model/App.php';
+		$app = file_get_contents(BP.'/'.$src);
+		if (!str_contains($app, '$sentry = new Luigifab_Sentry_Model_Client();'))
+			return '<div class="comment"><strong>INCOMPLETE MODULE INSTALLATION!</strong> Changes in <em>'.$src.'</em> are not present.<br />'.$msg.'</div>';
+
+		global $sentry;
+		if (!$sentry || !is_object($sentry))
+			return '<div class="comment"><strong>INCOMPLETE MODULE INSTALLATION!</strong> The <em>$sentry</em> global variable is empty.<br />'.$msg.'</div>';
+
+		return '<div class="comment">'.$msg.'</div>';
+	}
+}
